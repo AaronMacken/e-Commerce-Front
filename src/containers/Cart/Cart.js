3ae9c0d757e0d9749ae107a3936f2895fe2c13ae
Aaron Macken <aaron.a.macken@gmail.com>
2019-11-30 19:14:28 -0800

    Refactor the cart methods to go in the cart component instead of the checkout. pass data in to checkout component as props

--- src/containers/Cart/Cart.js
+++ src/containers/Cart/Cart.js
@@ -7,7 +7,48 @@ import LandingButton from "../../components/LandingButton/LandingButton";
 import "./Cart.css";
 
 class Cart extends Component {
+
+  // Get unique array names along with the number of occurences
+  getDisplayData(reduxState) {
+    // create an array with each of the redux element strings
+    let itemArray = reduxState.map(item => item.data.title);
+
+    // create associative array object
+    let newArr = {};
+
+    // assign key value pairs to that object. 
+    // Key being the array value and the value being the number of occurences
+    itemArray.forEach(i => {
+      newArr[i] = (newArr[i] || 0) + 1;
+    });
+
+    // create new array to store key value pairs from the associative array object
+    let orderArr = [];
+    for (let item in newArr) {
+      orderArr.push(`${item} x${newArr[item]}`);
+    }
+
+    // return that array to be used for displaying data and sending data to back end
+    return orderArr;
+  }
+
+  // use array returned from previous function along with the join method to create string to send to back end
+  getOrderString(reduxState) {
+    return this.getDisplayData(reduxState).join(", ");
+  }
+
+  // get order price fn
+  // add all item prices to a total variable and return that var
+  getOrderPrice(reduxState) {
+    let total = 0;
+    reduxState.forEach(item => {
+      total += item.data.price;
+    });
+    return total;
+  }
+
   render() {
+    // CART ITEMS FROM REDUX STATE
     let cartItems = this.props.checkoutItems.map((item, index) => (
       <CartItem
         title={item.data.title}
@@ -16,6 +57,9 @@ class Cart extends Component {
         index={index}
       />
     ));
+
+
+    // ------------------------------ RENDER EMPTY CART COMPONENT ------------------------- //
     if (this.props.checkoutItems.length < 1) {
       return (
         <div className="cart-page">
@@ -32,14 +76,45 @@ class Cart extends Component {
           </div>
         </div>
       );
+
+
+
+      // ------------------------------- RENDER FULL CART COMPONENT --------------------------- // 
     } else {
       return (
         <div className="cart-page">
           <Title text={"Shopping Cart"} />
           <div className="cart-col-wrapper">
             <div className="cart-col">
-              <Checkout />
+
+              {/* LEFT TOP */}
+              <div className="col-left-top">
+                {/* display data */}
+                <h2 className="sub-total">Order</h2>
+                <ul className="cart-items-list">
+                  {/* use getDisplayData function with map to return jsx elements of the returned array */}
+                  {this.getDisplayData(this.props.checkoutItems).map((item, index) => {
+                    return <li key={index} className="cart-item-li">{item}</li>;
+                  })}
+                </ul>
+              </div>
+
+
+              {/* LEFT BOTTOM */}
+              <div className="col-left-bottom">
+                <h2 className="sub-total">
+                  {/* Display total, round deciaml */}
+                  SubTotal: ${this.getOrderPrice(this.props.checkoutItems).toFixed(2)}
+                </h2>
+              </div>
+              <Checkout
+                stripeKey=""
+                amount={this.getOrderPrice(this.props.checkoutItems)}
+                name={this.getOrderString(this.props.checkoutItems)}
+              />
             </div>
+
+            {/* RIGHT SIDE COLUMN */}
             <div className="cart-col-right">{cartItems}</div>
           </div>
         </div>
--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -17,10 +17,10 @@ class Checkout extends React.Component {
     // await the post request to assign the value of that to a variable
 
     const cartPayload = {
-      orderString: this.getOrderString(this.props.checkoutItems),
-      totalPrice: this.getOrderPrice(this.props.checkoutItems)
+      orderString: this.props.name,
+      totalPrice: this.props.amount
     };
-    
+
     const response = await axios.post("/charge", {
       token,
       checkoutItems: cartPayload
@@ -34,81 +34,20 @@ class Checkout extends React.Component {
     }
   }
 
-  // =================================================================================================
 
-  // Get unique array names along with the number of occurences
-  getDisplayData(reduxState) {
-    // create an array with each of the redux element strings
-    let itemArray = reduxState.map(item => item.data.title);
-
-    // create associative array object
-    let newArr = {};
-
-    // assign key value pairs to that object. 
-    // Key being the array value and the value being the number of occurences
-    itemArray.forEach(i => {
-      newArr[i] = (newArr[i] || 0) + 1;
-    });
-
-    // create new array to store key value pairs from the associative array object
-    let orderArr = [];
-    for (let item in newArr) {
-      orderArr.push(`${item} x${newArr[item]}`);
-    }
-
-    // return that array to be used for displaying data and sending data to back end
-    return orderArr;
-  }
-
-  // use array returned from previous function along with the join method to create string to send to back end
-  getOrderString(reduxState) {
-    return this.getDisplayData(reduxState).join(", ");
-  }
-
-  // get order price fn
-  // add all item prices to a total variable and return that var
-  getOrderPrice(reduxState) {
-    let total = 0;
-    reduxState.forEach(item => {
-      total += item.data.price;
-    });
-    return total;
-  }
 
   render() {
     return (
       <div className="checkout-wrapper">
-        <div className="col-left-top">
-          {/* display data */}
-          <h2 className="sub-total">Order</h2>
-          <ul className="cart-items-list">
-            {/* use getDisplayData function with map to return jsx elements of the returned array */}
-            {this.getDisplayData(this.props.checkoutItems).map((item, index) => {
-              return <li key={index} className="cart-item-li">{item}</li>;
-            })}
-          </ul>
-        </div>
-
-
-
-        <div className="col-left-bottom">
-          <h2 className="sub-total">
-            {/* Display total, round deciaml */}
-            SubTotal: ${this.getOrderPrice(this.props.checkoutItems).toFixed(2)}
-          </h2>
-
-          {/* Stripe component that sends data to back end using methods combined with redux state */}
-          <StripeCheckout
-            stripeKey="" // stripe pk here
-            token={this.onToken}
-            billingAddress
-            shippingAddress
-            // multiply dollar amounts by 100 to convert to cents
-            amount={this.getOrderPrice(this.props.checkoutItems) * 100}
-            name={this.getOrderString(this.props.checkoutItems)}
-          />
-        </div>
-
+        <StripeCheckout
+          stripeKey={this.props.stripeKey} // stripe pk here
+          token={this.onToken}
+          billingAddress
+          shippingAddress
+          // multiply dollar amounts by 100 to convert to cents
+          amount={this.props.amount * 100}
+          name={this.props.name}
+        />
       </div>
     );
   }
