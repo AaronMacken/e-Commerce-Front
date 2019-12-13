3e69ad2f2e8e5f90d20fe034500a734a06f0c26e
Aaron Macken <aaron.a.macken@gmail.com>
2019-12-12 16:19:13 -0800

    QtySelector css additions

--- src/components/QtySelector/QtySelector.js
+++ src/components/QtySelector/QtySelector.js
@@ -1,43 +1,50 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import "./QtySelector.css";
 
 export default class QtySelector extends Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            numberOfItems: 1
-        }
+    this.state = {
+      numberOfItems: 1
+    };
 
-        this.handleInputIncrement = this.handleInputIncrement.bind(this);
-        this.handleInputDecrement = this.handleInputDecrement.bind(this);
+    this.handleInputIncrement = this.handleInputIncrement.bind(this);
+    this.handleInputDecrement = this.handleInputDecrement.bind(this);
+  }
+
+  handleInputIncrement(event) {
+    this.setState({ numberOfItems: this.state.numberOfItems + 1 });
+  }
+
+  handleInputDecrement(event) {
+    this.setState({ numberOfItems: this.state.numberOfItems - 1 });
+    if (this.state.numberOfItems < 2) {
+      this.setState({ numberOfItems: 1 });
     }
+  }
 
-    handleInputIncrement(event) {
-        this.setState({ numberOfItems: this.state.numberOfItems + 1});
-    }
+  render() {
+    return (
+      <div className="qty-selector-wrapper">
+        <button
+          className="chevron-left chevron"
+          onClick={this.handleInputDecrement}
+        >-</button>
 
-    handleInputDecrement(event) {
-        this.setState({ numberOfItems: this.state.numberOfItems - 1});
-        if(this.state.numberOfItems < 2) {
-            this.setState({numberOfItems: 1})
-        }
-    }
-
-    render() {
-        return (
-            <div className="qty-selector-wrapper">
-                <button onClick={this.handleInputDecrement}>Dec</button>
-                <form>
-                    <input type="number" 
-                    placeholder="Qty"
-                    readOnly
-                        className="qty-input"
-                        value={this.state.numberOfItems}
-                        ></input>
-                </form>
-                <button onClick={this.handleInputIncrement}>Inc</button>
-            </div>
-        )
-    }
+        <input
+          type="number"
+          placeholder="Qty"
+          readOnly
+          className="qty-input"
+          value={this.state.numberOfItems}
+        ></input>
+        
+        <button
+          className="chevron-right chevron"
+          onClick={this.handleInputIncrement}
+        >+</button>
+      </div>
+    );
+  }
 }
