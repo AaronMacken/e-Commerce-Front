72602a6c3bf5de4748dfc184cee71ad60f2724fa
Aaron Macken <aaron.a.macken@gmail.com>
2020-01-16 13:10:07 -0800

    git push heroku master

--- src/store/index.js
+++ src/store/index.js
@@ -17,8 +17,7 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 export const store = createStore(
   persistedReducer, 
   compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(...middleware)
   )
 );
 
