05ab0eaa53abe954df718afa34a2fdfa53c9c170
Aaron Macken <aaron.a.macken@gmail.com>
2020-01-24 19:46:08 -0800

    https redirect

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,26 +4,29 @@ import Main from "../containers/Main/Main";
 import Footer from "../components/Footer/Footer";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
-import {store} from "../store";
-import {persistor} from "../store";
+import { store } from "../store";
+import { persistor } from "../store";
 import { PersistGate } from "redux-persist/integration/react";
-import { ToastContainer} from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
+import HttpsRedirect from 'react-https-redirect'
 
 class App extends Component {
   render() {
     return (
-      <Provider store={store}>
-        <Router>
-          <PersistGate loading={null} persistor={persistor}>
-            <div className="App">
-              <Navbar />
-              <ToastContainer/>
-              <Main />
-              <Footer />
-            </div>
-          </PersistGate>
-        </Router>
-      </Provider>
+      <HttpsRedirect>
+        <Provider store={store}>
+          <Router>
+            <PersistGate loading={null} persistor={persistor}>
+              <div className="App">
+                <Navbar />
+                <ToastContainer />
+                <Main />
+                <Footer />
+              </div>
+            </PersistGate>
+          </Router>
+        </Provider>
+      </HttpsRedirect>
     );
   }
 }
