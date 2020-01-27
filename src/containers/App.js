1b6133f84513d29e08ddf82f0452ecc921577964
Aaron Macken <aaron.a.macken@gmail.com>
2020-01-27 09:22:09 -0800

    ScrollToTop component

--- src/containers/App.js
+++ src/containers/App.js
@@ -8,7 +8,8 @@ import { store } from "../store";
 import { persistor } from "../store";
 import { PersistGate } from "redux-persist/integration/react";
 import { ToastContainer } from 'react-toastify';
-import HttpsRedirect from 'react-https-redirect'
+import HttpsRedirect from 'react-https-redirect';
+import ScrollToTop from '../components/ScrollToTop/ScrollToTop';
 
 class App extends Component {
   render() {
@@ -17,12 +18,13 @@ class App extends Component {
         <Provider store={store}>
           <Router>
             <PersistGate loading={null} persistor={persistor}>
-              <div className="App">
-                <Navbar />
-                <ToastContainer />
-                <Main />
-                <Footer />
-              </div>
+              <ScrollToTop />
+                <div className="App">
+                  <Navbar />
+                  <ToastContainer />
+                  <Main />
+                  <Footer />
+                </div>
             </PersistGate>
           </Router>
         </Provider>
